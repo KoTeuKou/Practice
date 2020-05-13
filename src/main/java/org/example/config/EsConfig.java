609e6b299bf58b7f9a797a13a77dec8982c55f49
Daniil Shatalov <DaniilShat@yandex.ru>
2020-05-13 22:26:23 +0400

    Delete KOCTbI/\u

--- src/main/java/org/example/config/EsConfig.java
+++ src/main/java/org/example/config/EsConfig.java
@@ -1,7 +1,6 @@
 package org.example.config;
 
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -2,7 +2,6 @@ package org.example.controller;
 
 import org.example.domain.User;
 import org.example.service.UserService;
-import org.json.JSONException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -53,14 +51,14 @@ public class MainController {
     }
 
     @PostMapping("search")
-    public String getUsersBy(String param, String reqString, double cutoff_frequency, Model model) throws IOException, JSONException {
+    public String getUsersBy(String param, String reqString, double cutoff_frequency, Model model) {
         List<User> users = userService.getAccountsBy(param, reqString, cutoff_frequency);
         model.addAttribute("userList", users);
         return "redirect:/users";
     }
 
     @PostMapping("save")
-    public String save(User user, Model model) throws IOException, JSONException {
+    public String save(User user, Model model) {
         user.generateId();
         userService.save(user);
         List<User> accounts = userService.getAllAccounts();
